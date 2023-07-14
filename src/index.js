352e070791923e91e66ee35adef4bcd49e28465b
AlixiaDae <lizziejones1208@gmail.com>
2023-07-14 16:56:14 +0800

    Create function for creating objEls
    Add func for clearing missions and objs

--- src/index.js
+++ src/index.js
@@ -9,28 +9,63 @@ const UI = (() => {
     
     //Dummy Missions
     let mission = new Mission("Testing mission")
-    let mission2 = new Mission("2")
-    let mission3 = new Mission("3")
-    test.addMission(mission)
-    test.addMission(mission2)
-    test.addMission(mission3)
 
     //Dummy Objectives
     let objective = new Objective("Testing objective", "this is for testing")
-    test.addObjective(mission.getName(), objective)
+    test.addObjective("Today", objective)
 
     const missionsBox = document.querySelector(".missions")
+    const objectivesBox = document.querySelector(".objectives")
 
+    //this function takes mission object so element texts must be taken from its properties
     function createMissionElement(mission) {
         const missionElement = document.createElement("li")
         const missionText = document.createElement("h3")
         missionText.textContent = mission.getName()
+
+    //TODO create listener to render objectives for the mission
 
         missionElement.appendChild(missionText)
 
         return missionElement
     }
 
-    console.log(test.getQuestMenu())
+    //this function takes objective object so element texts must be taken from its properties 
+    function createObjectiveElement(objective) {
+        const objectiveElement = document.createElement("li")
+        const objectiveText = document.createElement("h4")
+        objectiveText.textContent = objective.getName()
+        
+        objectiveElement.appendChild(objectiveText)
+
+        return objectiveElement
+    }
+
+    function clearMissions() {
+        missionsBox.textContent = ""
+    }
+
+    function clearObjectives() {
+        objectivesBox.textContent = ""
+    }
+
+    //Rendering functions
+    function renderObjectives(mission) {
+        let objectivesArray = mission.getObjectives()
+        for(let objective of objectivesArray) {
+            objectivesBox.appendChild(createObjectiveElement(objective))
+        }
+    }
+
+    function renderMissions() {
+        let missionsArray = test.getQuestMenu().missions
+        for(let mission of missionsArray) {
+            missionsBox.appendChild(createMissionElement(mission))
+            renderObjectives(mission)
+        }
+    }
+
+    renderMissions()
+    console.log(test.getQuestMenu().missions)
     return main
 })()
