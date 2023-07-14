91bb136eb502e9ceeda87517e8d58b7e48a55609
AlixiaDae <lizziejones1208@gmail.com>
2023-07-14 17:49:31 +0800

    Add renderToday for default page load
    Add date-fns for later functions

--- src/index.js
+++ src/index.js
@@ -12,9 +12,14 @@ const UI = (() => {
     let mission2 = new Mission("Trying")
     test.addMission(mission2)
 
+    //TODO function checkDueDate() using date fns?
+    //TODO function moveObjectiveToToday()
+
     //Dummy Objectives
     let objective = new Objective("Testing objective", "this is for testing")
+    let todayObjective =new Objective("Test for Today", "todaaaay")
     test.addObjective("Today", objective)
+    test.addObjective("Today", todayObjective)
     let objective2 = new Objective("Trying empty message", "testing")
     test.addObjective("Trying", objective2)
     
@@ -22,14 +27,20 @@ const UI = (() => {
     const objectivesBox = document.querySelector(".objectives")
     const emptyMessage = document.querySelector(".empty-obj-message") 
 
+    const addMissionBtn = document.getElementById("add-mission")
+    const addObjectiveBtn = document.getElementById("add-objective")
+    //TODO add mission using button
+    //TODO create form for mission submission
+    //TODO add objective using button
+    //TODO create form for objective submission
+
     //this function takes mission object so element texts must be taken from its properties
     function createMissionElement(mission) {
         const missionElement = document.createElement("li")
         missionElement.classList.add("mission")
+        missionElement.setAttribute("role", "button")
         const missionText = document.createElement("h3")
         missionText.textContent = mission.getName()
-
-    //TODO create listener to render objectives for the mission
 
         missionElement.addEventListener("click", () => {
             renderObjectives(mission)
@@ -41,10 +52,17 @@ const UI = (() => {
     }
 
     //this function takes objective object so element texts must be taken from its properties 
+
     function createObjectiveElement(objective) {
         const objectiveElement = document.createElement("li")
+        objectiveElement.classList.add("objective")
         const objectiveText = document.createElement("h4")
         objectiveText.textContent = objective.getName()
+
+        //TODO add checkbox to delete objective after finished
+        //TODO add duedate
+        //TODO add listener to expand obj to show description
+        //TODO listener/button to edit name, description, and date of objective
         
         objectiveElement.appendChild(objectiveText)
 
@@ -81,7 +99,16 @@ const UI = (() => {
         }
     }
 
-    renderMissions()
+    //render Today's mission and objectives when page loads
+    function renderToday() {
+        renderMissions()
+        let today = test.getQuestMenu().getMission("Today").getObjectives()
+        for(let objective of today) {
+            objectivesBox.appendChild(createObjectiveElement(objective))
+        }
+    }
+
+    renderToday()
     console.log(test.getQuestMenu().missions)
     return main
 })()
