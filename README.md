f3dc6885b84e998936345e3518b6cf27db2e80bc
AlixiaDae <lizziejones1208@gmail.com>
2023-07-27 23:45:05 +0800

    Mission can now be deleted
    Note: Today and This Week missions cannot be deleted

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A Todo project for the OdinProject Javascript learning path.
+
+<a href="https://www.freepik.com/search?format=search&query=close%20button&type=icon">Icon by ariefstudio</a>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Mission from './Mission';
 import Objective from './Objective';
 import Storage from './Storage';
+import X from './close.png'
 
 const UI = (() => {
   const main = document.querySelector('.main');
@@ -14,24 +15,19 @@ const UI = (() => {
   // checks storage for objectives due today and moves them to Today's mission
   function checkStoredObjectives() {
     const storedTodos = test.getQuestMenu().getMissions()
-    console.log(storedTodos)
     for(const mission of storedTodos) {
       const missionObjectives = mission.getObjectives()
       for(const objective of missionObjectives) {
         if(isToday(objective)) {
-          console.log(`this ${objective.getName()} is for today`)
           const objName = objective.getName()
           const objDescription = objective.getDescription()
           const objDate = objective.getDateFormatted()
           const newObjective = new Objective(objName, objDescription, objDate)
-          console.log(objective.getName())
           test.deleteObjective(mission.getName(), objective.getName())
           test.addObjective("Today", newObjective)
-          console.log(mission)
         }
       }
     }
-    console.log(test.getQuestMenu())
   }
 
   // Check if objective is due today
@@ -41,7 +37,6 @@ const UI = (() => {
     const dateMonth = date.getMonth() + 1
     const dateYear = date.getFullYear()
     const fullDate = `${dateMonth}/${dateToday}/${dateYear}`
-    console.log(fullDate)
     if(obj.getDateFormatted() === "today" || obj.getDateFormatted() === fullDate) {
       return true
     } else {
@@ -66,7 +61,6 @@ const UI = (() => {
   const addObjectiveBtn = document.getElementById('add-objective');
   // TODO add mission using button
   
-  // TODO create form for mission submission
   const missionFormBox = document.createElement("div")
   missionFormBox.style.display = "none"
   missionFormBox.classList.add("mission-form-box")
@@ -126,14 +120,36 @@ const UI = (() => {
     const missionText = document.createElement('h3');
     missionText.textContent = mission.getName();
 
+    
+
     missionElement.addEventListener('click', () => {
       renderObjectives(mission);
     });
 
     missionElement.appendChild(missionText);
+    createDeleteBtn(mission.getName(), missionElement)
 
     return missionElement;
   }
+
+  function createDeleteBtn(missionName, parentElement) {
+    if(missionName !== "Today" && missionName !== "This Week") {
+      const deleteMissionBtn = document.createElement("button")
+      deleteMissionBtn.type = "button"
+      const deleteMissionImg = document.createElement("img")
+      deleteMissionImg.classList.add("delete-img")
+      deleteMissionImg.src = X
+
+      deleteMissionBtn.addEventListener("click", () => {
+        test.deleteMission(mission.getName())
+        renderMissions()
+      })
+      deleteMissionBtn.appendChild(deleteMissionImg)
+      parentElement.appendChild(deleteMissionBtn)
+    } 
+  }
+
+
 
   // creates objective element using objective object
   function createObjectiveElement(objective) {
