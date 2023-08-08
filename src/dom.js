f570d4e131d5fed6d93334b81cafd4fdb420f62c
AlixiaDae <lizziejones1208@gmail.com>
2023-08-09 00:43:54 +0800

    Missions now display their objective

--- src/dom.js
+++ src/dom.js
@@ -7,6 +7,17 @@ const quest = new Storage()
 const testMission = new Mission("Testing mission")
 quest.addMission(testMission)
 
+const testObjective = new Objective("Test Objective", "this is for testing", "2023-08-10")
+quest.addObjective("Today", testObjective)
+
+function handleClass(element,className,job) {
+    if(job === "remove") {
+        element.classList.remove(className)
+    } else {
+        element.classList.add(className)
+    }
+}
+
 function toggleForm(button) {
     if(button.target.textContent === "Add Mission") {
       document.querySelector(".mission-form").classList.toggle("show-form")
@@ -20,8 +31,9 @@ function toggleForm(button) {
 function createMissionElement(mission) {
     if(mission.getName() === "Today") {
         const missionElement = document.createElement("li")
+        missionElement.classList.add("mission-element")
         const missionIcon = document.createElement("span")
-        missionIcon.classList.add("fa-solid", "fa-map-pin", "no-hover")
+        missionIcon.classList.add("fa-solid", "fa-map-pin")
         const missionText = document.createElement("p")
         missionText.textContent = mission.getName()
         
@@ -29,6 +41,7 @@ function createMissionElement(mission) {
         return missionElement
     } else {
         const missionElement = document.createElement("li")
+        missionElement.classList.add("mission-element")
         const missionIcon = document.createElement("span")
         missionIcon.classList.add("fa-solid", "fa-map-pin")
         const missionText = document.createElement("p")
@@ -51,6 +64,31 @@ function createMissionElement(mission) {
     }
 }
 
+function createObjectiveElement(objective) {
+    const objectiveElement = document.createElement("li")
+    objectiveElement.classList.add("objective-element")
+
+    const objCheckbox = document.createElement("input")
+    objCheckbox.setAttribute("type", "checkbox")
+    objCheckbox.id = objective.getName()
+
+    const objCheckboxLabel = document.createElement("label")
+    objCheckboxLabel.setAttribute("for", objective.getName())
+
+    const objectiveBox = document.createElement("div")
+
+    const objectiveText = document.createElement("p")
+    objectiveText.textContent = objective.getName()
+
+    const objectiveDate = document.createElement("p")
+    objectiveDate.textContent = objective.getDateFormatted()
+
+    objectiveBox.append(objectiveText, objectiveDate)
+    objectiveElement.append(objCheckbox, objCheckboxLabel, objectiveBox)
+    return objectiveElement
+
+}
+
 function displayMissions(missionList) {
     const list = missionList
     const missionsListElement = document.querySelector(".missions-list")
@@ -63,17 +101,44 @@ function displayMissions(missionList) {
         // for each mission, createMissionElement()
 }
 
+function displayObjectives(mission) {
+    const objectivesElement = document.querySelector(".objectives")
+    const objectiveList = mission.getObjectives()
+    for(const objective of objectiveList) {
+        objectivesElement.appendChild(createObjectiveElement(objective))
+    }
+}
+
+function setActiveMission(mission) {
+    const objectivesElement = document.querySelector(".objectives")
+    if (mission.getObjectives().length === 0) {
+        clear(objectivesElement)
+        const noObjectivesElement = document.createElement("p")
+        noObjectivesElement.classList.add("no-objectives")
+        noObjectivesElement.textContent = "This mission has no objectives yet."
+        objectivesElement.appendChild(noObjectivesElement)
+    } else {
+        clear(objectivesElement)
+        displayObjectives(mission)
+    }
+    return objectivesElement
+}
+
 function clear(element) {
     element.textContent = ""
 }
 
 function render() {
     displayMissions(quest.getQuestMenu().getMissions())
+    setActiveMission(quest.getQuestMenu().getMission("Today"))
 }
 
 export {
+    handleClass,
     displayMissions,
     clear,
     toggleForm,
-    render
+    render,
+    setActiveMission,
+    quest
 }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,28 @@ const showFormButtons = document.querySelectorAll(".show-form-button")
 
 showFormButtons.forEach(button => { button.addEventListener("click", dom.toggleForm) })
 
+
+
+// when a li is clicked, add .pinned class to span pin icon
+// for each mission element, addEventListener clicked and add pinned class to pin
+
 dom.render()
+
+const missionElements = document.querySelectorAll(".mission-element")
+const missionTextElements = document.querySelectorAll(".mission-element p")
+const pins = document.querySelectorAll(".fa-map-pin")
+
+missionElements.forEach(mission => {
+    mission.addEventListener("click", (e) => {
+        missionElements.forEach(el => {
+            dom.handleClass(el, "active-mission", "remove")
+            pins.forEach(pin => {
+                dom.handleClass(pin, "pinned", "remove")
+            })
+        })
+        const missionName = e.target.textContent
+        dom.handleClass(mission, "active-mission", "add")
+        dom.handleClass(mission.children[0], "pinned", "add")
+        dom.setActiveMission(dom.quest.getQuestMenu().getMission(missionName))
+    })
+})
